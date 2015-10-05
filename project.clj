fc29b626f4fa0187c595145a81c6078f2076fa00
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-10-05 11:18:48 +0200

    Added dependent libraries

--- project.clj
+++ project.clj
@@ -1,9 +1,14 @@
 (defproject arachnida "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :main ^:skip-aot arachnida.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+    :description "FIXME: write description"
+    :url "http://example.com/FIXME"
+    :license {:name "Eclipse Public License"
+              :url "http://www.eclipse.org/legal/epl-v10.html"}
+    :dependencies [[org.clojure/clojure "1.6.0"]
+                   [org.clojure/java.jdbc "0.3.5"]
+                   [org.xerial/sqlite-jdbc "3.7.2"]
+                   [clj-jgit "0.8.0"]
+                   [clj-rm-rf "1.0.0-SNAPSHOT"]
+    ]
+    :main ^:skip-aot arachnida.core
+    :target-path "target/%s"
+    :profiles {:uberjar {:aot :all}})
