9f3ba718d7aadbc4a6a16cd2dbc24c64832aba84
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-10-05 11:24:51 +0200

    Ring profile for the project

--- project.clj
+++ project.clj
@@ -4,11 +4,16 @@
     :license {:name "Eclipse Public License"
               :url "http://www.eclipse.org/legal/epl-v10.html"}
     :dependencies [[org.clojure/clojure "1.6.0"]
-                   [org.clojure/java.jdbc "0.3.5"]
-                   [org.xerial/sqlite-jdbc "3.7.2"]
                    [clj-jgit "0.8.0"]
                    [clj-rm-rf "1.0.0-SNAPSHOT"]
-    ]
+                   [org.clojure/java.jdbc "0.3.5"]
+                   [org.xerial/sqlite-jdbc "3.7.2"]
+                   [hiccup "1.0.4"]
+                   [ring/ring-core "1.3.2"]
+                   [ring/ring-jetty-adapter "1.3.2"]]
+    :dev-dependencies [[lein-ring "0.8.10"]]
+    :plugins [[lein-ring "0.8.10"]]
     :main ^:skip-aot arachnida.core
     :target-path "target/%s"
+    :ring {:handler gitt.core/app}
     :profiles {:uberjar {:aot :all}})
