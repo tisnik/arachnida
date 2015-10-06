3b3c3dbfcd90acd5ff3361476a627dd56205883b
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-10-06 13:34:03 +0200

    Git script should be backward compatible with older GITs

--- scripts/get-commits-stat.sh
+++ scripts/get-commits-stat.sh
@@ -1,5 +1,5 @@
 lastdir=`pwd`
 cd $1
 git checkout $2
-git log --oneline --no-abbrev-commit --shortstat --no-merges > $lastdir/stats
+git log --pretty=oneline --no-abbrev --shortstat --no-merges > $lastdir/stats
 
