c679a64cceec9e8921ab93344f53d9714e9ec9fa
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-10-06 13:37:01 +0200

    New functions: read-commit-id, insert-changed-file, insert-commit

--- src/arachnida/db_interface.clj
+++ src/arachnida/db_interface.clj
@@ -15,3 +15,26 @@
                                   from   repos
                                   where  product=? order by id;" product-id]))
 
+(defn read-commit-id
+    [db product repo branch sha]
+    (:id
+    (first
+        (jdbc/query db ["select id from commits where product=? and repo=? and branch=? and sha=?"
+                             product repo branch sha]))))
+
+(defn insert-changed-file
+    [db commit-id changed-file]
+    (jdbc/insert! db "changed_files" {:commit_id commit-id
+                                           :file_name (first changed-file)
+                                           :operation (second changed-file)}))
+
+(defn insert-commit
+    "Zapis dat do tabulky."
+    [db product repo branch sha message author date files-changed insertions deletions]
+    (jdbc/insert! db "commits" {:product product :repo repo :branch branch
+                                             :sha     sha     :message message
+                                             :author author   :date date
+                                             :files_changed files-changed
+                                             :insertions insertions
+                                             :deletions deletions}))
+
