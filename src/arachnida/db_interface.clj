9d2fe6f1de9cf231b9f67e2670015f531db7ba32
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-10-08 12:07:32 +0200

    Added functions to read stats from DB

--- src/arachnida/db_interface.clj
+++ src/arachnida/db_interface.clj
@@ -32,9 +32,67 @@
     "Zapis dat do tabulky."
     [db product repo branch sha message author date files-changed insertions deletions]
     (jdbc/insert! db "commits" {:product product :repo repo :branch branch
-                                             :sha     sha     :message message
-                                             :author author   :date date
-                                             :files_changed files-changed
-                                             :insertions insertions
-                                             :deletions deletions}))
+                                :sha     sha     :message message
+                                :author author   :date date
+                                :files_changed files-changed
+                                :insertions insertions
+                                :deletions deletions}))
+
+(defn read-authors
+    []
+    (jdbc/query db-spec/data-db
+                               ["select author as author,
+                                        count(*) as commits_count,
+                                        sum(files_changed) as files_changed,
+                                        sum(insertions) as insertions,
+                                        sum(deletions) as deletions
+                                 from commits
+                                 where date between '2015-01-01' and '2015-12-31'
+                                 group by author order by author"]))
+
+(defn read-stat-per-weeks-from-db
+    [first-day last-day]
+    (jdbc/query db-spec/data-db ["select count(*) as commits_count,
+                                         sum(files_changed) as files_changed,
+                                         sum(insertions) as insertions,
+                                         sum(deletions) as deletions
+                                 from commits
+                                 where date between ? and ?" first-day last-day]))
+
+(defn read-stat-per-weeks-for-author-from-db
+    [first-day last-day author]
+    (jdbc/query db-spec/data-db ["select count(*) as commits_count,
+                                         sum(files_changed) as files_changed,
+                                         sum(insertions) as insertions,
+                                         sum(deletions) as deletions
+                                 from commits
+                                 where date between ? and ? and author=?" first-day last-day author]))
+
+(defn read-statistic-for-week-from-db
+    [first-day last-day author]
+    (jdbc/query db-spec/data-db ["select repo,
+                                         count(*) as commits,
+                                         sum(files_changed) as files,
+                                         sum(insertions) as insertions,
+                                         sum(deletions) as deletions
+                                 from  commits
+                                 where date between ? and ?
+                                   and author=?
+                                 group by repo
+                                 order by repo" first-day last-day author]))
+
+(defn read-commits-for-week
+    [first-day last-day author]
+    (jdbc/query db-spec/data-db ["select id,
+                                         (select name from products where products.id=commits.product) as product,
+                                         (select name from repos where repos.id=commits.repo) as repo,
+                                         sha, branch, message, date
+                                  from commits
+                                  where date between ? and ? and author=?
+                                  order by product, repo, date" first-day last-day author]))
+
+(defn read-commit-info
+    [commit-id]
+    (if commit-id
+        (jdbc/query db-spec/data-db ["select * from changed_files where commit_id=?" commit-id])))
 
