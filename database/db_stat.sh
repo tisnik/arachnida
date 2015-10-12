6df1d0c314af68a692d13c09c74c9023bc4186a9
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-10-12 11:09:30 +0200

    Script to show DB statistic

---
+++ database/db_stat.sh
@@ -0,0 +1,11 @@
+echo "Products:"
+sqlite3 ../data.db "select count(*) from products;"
+echo "Repos:"
+sqlite3 ../data.db "select count(*) from repos;"
+echo "Commits:"
+sqlite3 ../data.db "select count(*) from commits;"
+echo "Branches:"
+sqlite3 ../data.db "select count(*) from branches_for_commit;"
+echo "Changed files:"
+sqlite3 ../data.db "select count(*) from changed_files;"
+
