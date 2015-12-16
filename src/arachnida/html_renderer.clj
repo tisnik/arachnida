7def13b852a1c1972f97a03649951a6cc5a32307
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-12-16 17:06:48 +0100

    render-html-header and render-html-footer

--- src/arachnida/html_renderer.clj
+++ src/arachnida/html_renderer.clj
@@ -1,2 +1,23 @@
 (ns arachnida.html-renderer)
 
+(defn render-html-header
+    "Renders part of HTML page - the header."
+    []
+    [:head
+        [:title "Arachnida"]
+        [:meta {:name "Author"    :content "Pavel Tisnovsky"}]
+        [:meta {:name "Generator" :content "Clojure"}]
+        [:meta {:http-equiv "Content-type" :content "text/html; charset=utf-8"}]
+        ;(page/include-css "http://torment.usersys.redhat.com/openjdk/style.css")]
+        (page/include-css "bootstrap.min.css")
+        (page/include-css "smearch.css")
+        (page/include-js  "bootstrap.min.js")
+    ] ; head
+)
+
+(defn render-html-footer
+    "Renders part of HTML page - the footer."
+    []
+    [:div (str "<br /><br /><br /><br />Author: Pavel Tisnovsky &lt;<a href='mailto:ptisnovs@redhat.com'>ptisnovs@redhat.com</a>&gt;&nbsp;&nbsp;&nbsp;"
+          "<br />")])
+
