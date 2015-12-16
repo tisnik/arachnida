590c80c32bb4c17c0d05f59e37457a281a5f1623
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-12-16 14:32:59 +0100

    Better output for list_db.sh script

--- database/list_db.sh
+++ database/list_db.sh
@@ -1,5 +1,15 @@
+echo "Products:"
 sqlite3 ../data.db "select * from products;"
+
+echo "Repos:"
 sqlite3 ../data.db "select * from repos;"
+
+echo "Commits:"
 sqlite3 ../data.db "select * from commits;"
+
+echo "Branches:"
+sqlite3 ../data.db "select * from branches_for_commit;"
+
+echo "Changed files:"
 sqlite3 ../data.db "select * from changed_files;"
 
