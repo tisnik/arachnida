2e2217cb45a7596d8fa87a2cde47128dac9784d9
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-12-16 17:07:20 +0100

    Hiccup imports

--- src/arachnida/html_renderer.clj
+++ src/arachnida/html_renderer.clj
@@ -1,5 +1,9 @@
 (ns arachnida.html-renderer)
 
+(require '[hiccup.core                   :as hiccup])
+(require '[hiccup.page                   :as page])
+(require '[hiccup.form                   :as form])
+
 (defn render-html-header
     "Renders part of HTML page - the header."
     []
@@ -9,9 +13,12 @@
         [:meta {:name "Generator" :content "Clojure"}]
         [:meta {:http-equiv "Content-type" :content "text/html; charset=utf-8"}]
         ;(page/include-css "http://torment.usersys.redhat.com/openjdk/style.css")]
-        (page/include-css "bootstrap.min.css")
-        (page/include-css "smearch.css")
-        (page/include-js  "bootstrap.min.js")
+        (page/include-css "http://10.34.3.139/bootstrap.min.css")
+        (page/include-js  "http://10.34.3.139/bootstrap.min.js")
+        [:script {:type "text/javascript" :src "http://10.34.3.139/flotr/lib/prototype-1.6.0.2.js"}]
+        [:script {:type "text/javascript" :src "http://10.34.3.139/flotr/lib/canvas2image.js"}]
+        [:script {:type "text/javascript" :src "http://10.34.3.139/flotr/lib/canvastext.js"}]
+        [:script {:type "text/javascript" :src "http://10.34.3.139/flotr/flotr.debug-0.2.0-alpha.js"}]
     ] ; head
 )
 
