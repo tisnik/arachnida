ab1668215408cad96044e88e459ef549e1674f2a
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-12-16 14:38:45 +0100

    New function: load-repositories

--- src/arachnida/config.clj
+++ src/arachnida/config.clj
@@ -2,3 +2,8 @@
 
 (require '[clojure-ini.core :as clojure-ini])
 
+(defn load-repositories
+    "Load repositories from the provided INI file."
+    [ini-file-name]
+    (clojure-ini/read-ini ini-file-name :keywordize? true))
+
