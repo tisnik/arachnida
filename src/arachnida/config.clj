e680b72e0efba5285e2bc42c4f32521127d41632
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-12-17 11:39:46 +0100

    Function to load global configuration and set URL to common files

--- src/arachnida/config.clj
+++ src/arachnida/config.clj
@@ -5,8 +5,21 @@
 (def repositories-ini-file
     "config/repositories.ini")
 
+(def config-ini-file
+    "config/config.ini")
+
+(def url-to-common-files
+    (atom nil))
+
 (defn load-repositories
     "Load repositories from the provided INI file."
     []
     (clojure-ini/read-ini repositories-ini-file :keywordize? true))
+
+(defn load-configuration
+    "Load configuration from the provided INI file."
+    []
+    (let [cfg (clojure-ini/read-ini config-ini-file :keywordize? true)
+          settings (:settings cfg)]
+        (reset! url-to-common-files (:url-to-common-files settings))))
 
