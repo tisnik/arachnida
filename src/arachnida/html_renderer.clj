5546c23f29e636feadd25b9d9a34e022b3bc8cb2
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-12-17 11:45:39 +0100

    Proper use of common files on index page

--- src/arachnida/html_renderer.clj
+++ src/arachnida/html_renderer.clj
@@ -1,8 +1,10 @@
 (ns arachnida.html-renderer)
 
-(require '[hiccup.core                   :as hiccup])
-(require '[hiccup.page                   :as page])
-(require '[hiccup.form                   :as form])
+(require '[hiccup.core         :as hiccup])
+(require '[hiccup.page         :as page])
+(require '[hiccup.form         :as form])
+
+(require '[arachnida.config    :as config])
 
 (defn render-html-header
     "Renders part of HTML page - the header."
@@ -12,14 +14,13 @@
         [:meta {:name "Author"    :content "Pavel Tisnovsky"}]
         [:meta {:name "Generator" :content "Clojure"}]
         [:meta {:http-equiv "Content-type" :content "text/html; charset=utf-8"}]
-        (page/include-css "bootstrap.min.css")
-        (page/include-css "bootstrap.min.css")
-        (page/include-css "arachnida.css")
-        (page/include-js  "bootstrap.min.js")
-        (page/include-js  "flotr/lib/prototype-1.6.0.2.js")
-        (page/include-js  "flotr/lib/canvas2image.js")
-        (page/include-js  "flotr/lib/canvastext.js")
-        (page/include-js  "flotr/flotr.debug-0.2.0-alpha.js")
+        (page/include-css (str @config/url-to-common-files "bootstrap.min.css"))
+        (page/include-css (str @config/url-to-common-files "arachnida.css"))
+        (page/include-js  (str @config/url-to-common-files "bootstrap.min.js"))
+        (page/include-js  (str @config/url-to-common-files "flotr/lib/prototype-1.6.0.2.js"))
+        (page/include-js  (str @config/url-to-common-files "flotr/lib/canvas2image.js"))
+        (page/include-js  (str @config/url-to-common-files "flotr/lib/canvastext.js"))
+        (page/include-js  (str @config/url-to-common-files "flotr/flotr.debug-0.2.0-alpha.js"))
     ] ; head
 )
 
@@ -70,6 +71,51 @@
                 [:table {:class "table table-condensed table-hover table-bordered" :rules "all"}
                     (for [writer writers]
                         [:tr [:td [:a {:href (str "/author?name=" (:author writer)) } (:author writer)]]])
+                ]
+                [:br][:br][:br][:br]
+                (render-html-footer)
+            ] ; </div class="container">
+        ] ; </body>
+    ))
+
+(defn render-product-page
+    [product-name repositories]
+    (page/xhtml
+        (render-html-header)
+        [:body
+            [:div {:class "container"}
+                (render-navigation-bar-section)
+                [:h1 (str "Product: " product-name)]
+                [:table {:class "table table-condensed table-hover table-bordered" :rules "all"}
+                    (for [repository repositories]
+                        [:tr
+                            [:td (:name repository)]
+                            [:td (:url repository)]])
+                ]
+                [:br][:br][:br][:br]
+                (render-html-footer)
+            ] ; </div class="container">
+        ] ; </body>
+    ))
+
+(defn render-author-page
+    [author-name statistic]
+    (page/xhtml
+        (render-html-header)
+        [:body
+            [:div {:class "container"}
+                (render-navigation-bar-section)
+                [:h1 (str "Author: " author-name)]
+                [:table {:class "table table-condensed table-hover table-bordered" :rules "all"}
+                    [:tr [:th {:colspan 2} "Summary for 2015"]]
+                    [:tr [:td "Commits:"]
+                         [:td (:commits_count statistic)]]
+                    [:tr [:td "Insertions:"]
+                         [:td (:insertions statistic)]]
+                    [:tr [:td "Deletions:"]
+                         [:td (:deletions statistic)]]
+                    [:tr [:td "Changed files:"]
+                         [:td (:files_changed statistic)]]
                 ]
                 [:br][:br][:br][:br]
                 (render-html-footer)
