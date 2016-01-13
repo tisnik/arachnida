18e80eacfb8e02d8831b19b491441506648cbf8b
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-01-13 15:33:03 +0100

    New configuration options: start_year and end_year

--- src/arachnida/config.clj
+++ src/arachnida/config.clj
@@ -29,6 +29,12 @@
 (def port
     (atom nil))
 
+(def start-year
+    (atom nil))
+
+(def end-year
+    (atom nil))
+
 (defn load-repositories
     "Load repositories from the provided INI file."
     []
@@ -37,9 +43,10 @@
 (defn load-configuration
     "Load configuration from the provided INI file."
     []
-    (let [cfg (clojure-ini/read-ini config-ini-file :keywordize? true)
-          settings (:settings cfg)]
-        (reset! url-to-common-files (:url-to-common-files settings))
-        (reset! mailto (:mailto settings))
-        (reset! port (Integer/parseInt (-> cfg :server :port)))))
+    (let [cfg (clojure-ini/read-ini config-ini-file :keywordize? true)]
+        (reset! url-to-common-files (-> cfg :settings :url-to-common-files))
+        (reset! mailto     (-> cfg :settings :mailto))
+        (reset! port       (Integer/parseInt (-> cfg :server :port)))
+        (reset! start-year (Integer/parseInt (-> cfg :calendar :start_year)))
+        (reset! end-year   (Integer/parseInt (-> cfg :calendar :end_year)))))
 
