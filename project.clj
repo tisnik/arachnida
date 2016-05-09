417438711468359757a8ff51f75a76524f0c1e29
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-05-09 17:49:58 +0200

    Updated projects: codox, lein-cloverage, and junit-plugin libraries

--- project.clj
+++ project.clj
@@ -19,17 +19,21 @@
                    [clj-jgit "0.8.0"]
                    [clj-rm-rf "1.0.0-SNAPSHOT"]
                    [org.clojure/java.jdbc "0.3.5"]
-                   [org.xerial/sqlite-jdbc "3.7.2"]
                    [org.clojure/tools.cli "0.3.1"]
+                   [org.xerial/sqlite-jdbc "3.7.2"]
+                   [clojure-ini "0.0.1"]
                    [ring/ring-core "1.3.2"]
                    [ring/ring-jetty-adapter "1.3.2"]
-                   [clojure-ini "0.0.1"]
                    [org.clojars.tisnik/clj-flotr "0.2.0-SNAPSHOT"]
                    [org.clojars.tisnik/clj-calendar "0.1.0-SNAPSHOT"]
                    [hiccup "1.0.4"]]
     :dev-dependencies [[lein-ring "0.8.10"]]
-    :plugins [[lein-ring "0.8.10"]]
+    :plugins [[lein-ring "0.8.10"]
+              [codox "0.8.11"]
+              [test2junit "1.1.0"]
+              [lein-cloverage "1.0.6"]]
     :main ^:skip-aot arachnida.core
     :target-path "target/%s"
     :ring {:handler arachnida.server/app}
     :profiles {:uberjar {:aot :all}})
+
